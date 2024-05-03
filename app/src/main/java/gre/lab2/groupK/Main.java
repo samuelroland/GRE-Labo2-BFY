5062b02f25968a9f55ebfd24e4e8b9aeba7fe27e
Simon <simon.mehicot@gmail.com>
2024-05-03 17:21:56 +0200

    Bases du main, peut être des erreurs avec gradle

--- app/src/main/java/gre/lab2/groupK/Main.java
+++ app/src/main/java/gre/lab2/groupK/Main.java
@@ -1,13 +1,22 @@
 /*
- * Authors: Samuel Roland, Julien Mühlemann, Simon Menicot
+ * Authors: Samuel Roland, Simon Menicot
  */
 package gre.lab2.groupK;
+
+import gre.lab2.graph.*;
 
 import java.io.IOException;
 
 public final class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("test");
+        System.out.println("Labo 2 - GRE");
+
+        final String[] FILE_INDEXES = {"1","2","3","4"};
+        for (var i : FILE_INDEXES){
+            System.out.println("Calcul des arborescences de plus court chemin dans le réseau "+ i + ".");
+
+            WeightedDigraph graph = WeightedDigraphReader.fromFile("data/reseau" + i + ".txt");
+        }
         // TODO
         // - Renommage du package ;
         // - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
