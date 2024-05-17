d4ef40105660765b1eca4c82ce7032d4d1a6ecc2
Samuel Roland <samuelroland@noreply.codeberg.org>
2024-05-17 15:48:39 +0200

    Try to detect circuit absorbent

--- app/src/main/java/gre/lab2/groupK/BellmanFordYensAlgorithm.java
+++ app/src/main/java/gre/lab2/groupK/BellmanFordYensAlgorithm.java
@@ -3,6 +3,9 @@ package gre.lab2.groupK;
 import java.util.AbstractQueue;
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 import gre.lab2.graph.BFYResult;
@@ -21,7 +24,7 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
         Arrays.fill(distsTo, Integer.MAX_VALUE);
         Arrays.fill(preds, -1);
         distsTo[from] = 0;
-        int iterationCount = 0;
+        int iterationCount = 1; // iteration number (from 1 to nVertices for nVertices - 1 iterations)
 
         // TODO: check container and default capacity
         ArrayDeque<Integer> verticesQueue = new ArrayDeque<>(); // queue of vertices number to look at at the next
@@ -29,15 +32,14 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
         // Add the source and sentinelle in the queue for the first iteration
         verticesQueue.addFirst(from);
         verticesQueue.addFirst(SENTINEL);
-
+        boolean lastIteration = false;
         while (!verticesQueue.isEmpty()) {
             int first = verticesQueue.removeLast(); // TODO: or .remove() ?
             if (first == SENTINEL) {
                 if (!verticesQueue.isEmpty()) {
 
-                    if (++iterationCount >= nVertices) {
-                        return null;
-                        // TODO: serach for absorbant circuit
+                    if (iterationCount++ == nVertices) {
+                        lastIteration = true;
                     } else {
                         verticesQueue.addFirst(SENTINEL);
                     }
@@ -50,8 +52,28 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
                         distsTo[to] = newDistance;
                         preds[to] = first;
 
+                        // If the last iteration brought some amelioration, we have to look for
+                        // the absorbent circuit
+                        if (lastIteration) {
+                            // List<Integer> stack = new LinkedList<Integer>();
+                            // stack(first);
+                            // int i = first;
+                            // while (!stack.contains(preds[i])) {
+                            // stack.push(preds[i]);
+                            // i = preds[i];
+                            // }
+                            // TODO: implement boolean array to O(M) (M is number of discovery vertices)
+                            //TODO: ask assistant if okay to put time complexity before spatial complexity
+                            // while(stack.)
+                            // List<Integer> circuitVertices =
+                            // return new BFYResult.NegativeCycle();
+
+                        }
+
                         if (!verticesQueue.contains(to))
                             verticesQueue.addFirst(to);
+                    } else if (lastIteration) {
+                        return new BFYResult.ShortestPathTree(distsTo, preds);
                     }
                 }
             }
