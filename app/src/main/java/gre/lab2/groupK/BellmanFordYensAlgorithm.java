e8d905130a3009ad34880b143a1bb27ae2ea43bf
Simon <simon.mehicot@gmail.com>
2024-05-24 16:34:09 +0200

    komanterrent

--- app/src/main/java/gre/lab2/groupK/BellmanFordYensAlgorithm.java
+++ app/src/main/java/gre/lab2/groupK/BellmanFordYensAlgorithm.java
@@ -1,49 +1,56 @@
 package gre.lab2.groupK;
 
-import java.util.AbstractQueue;
 import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 import gre.lab2.graph.BFYResult;
 import gre.lab2.graph.IBellmanFordYensAlgorithm;
 import gre.lab2.graph.WeightedDigraph;
 import gre.lab2.graph.WeightedDigraph.Edge;
 
+/**
+ * Implements the Bellman-Ford-Yens's algorithm
+ */
 public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm {
     static final int SENTINEL = -1; // sentinel value in the queue
     static final int VIEWED = -2; // to put in preds when the vertex is found
+    static final int NO_PRED = -1; // in the predecessor list, when there is no predecessor
 
+    /**
+     * Apply the algorithm to a weighted directed graph
+     * @param graph the graph on witch to found the shortest paths
+     * @param from the vertex of the graph from which the shortest paths have to be found
+     * @return the result of the algorithm, see {@link BFYResult}
+     */
     @Override
     public BFYResult compute(WeightedDigraph graph, int from) {
 
         int nVertices = graph.getNVertices();
         int[] distsTo = new int[nVertices]; // distance from the source to vertex i
         int[] preds = new int[nVertices]; // predecessor of vertex i
-        boolean[] contains = new boolean[nVertices];
 
-        Arrays.fill(distsTo, Integer.MAX_VALUE);
-        Arrays.fill(preds, -1);
+        Arrays.fill(distsTo, Integer.MAX_VALUE); // to simulate infinity
+        Arrays.fill(preds, NO_PRED);
         distsTo[from] = 0;
         int iterationCount = 1; // iteration number (from 1 to nVertices for nVertices - 1 iterations)
 
         // TODO: check container and default capacity
         ArrayDeque<Integer> verticesQueue = new ArrayDeque<>(); // queue of vertices number to look at at the next
-                                                                // iteration
+        // iteration
+        boolean[] contains = new boolean[nVertices]; // to see in O(1) if a vertex is in verticesQueue
         // Add the source and sentinelle in the queue for the first iteration
         verticesQueue.addFirst(from);
         verticesQueue.addFirst(SENTINEL);
         contains[from] = true;
-        boolean lastIteration = false;
+        boolean lastIteration = false; // true if it is the nth iteration
         while (!verticesQueue.isEmpty()) {
-            int first = verticesQueue.removeLast(); // TODO: or .remove() ?
-            if (first == SENTINEL) {
+            int first = verticesQueue.removeLast(); // first is the current vertex
+            if (first == SENTINEL) { // if we are at the end of an iteration
                 if (!verticesQueue.isEmpty()) {
 
-                    if (iterationCount++ == nVertices) { // TODO: iter++ or ++iter ?
+                    if (iterationCount++ == nVertices) { // if we're starting the nth iteration
                         lastIteration = true;
                     } else {
                         verticesQueue.addFirst(SENTINEL);
@@ -51,10 +58,10 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
                 }
             } else {
                 contains[first] = false;
-                for (Edge outgoingEdge : graph.getOutgoingEdges(first)) {
+                for (Edge outgoingEdge : graph.getOutgoingEdges(first)) { // for each outgoing edge of the current vetex
                     int newDistance = distsTo[first] + outgoingEdge.weight();
                     int to = outgoingEdge.to();
-                    if (distsTo[to] > newDistance) {
+                    if (distsTo[to] > newDistance) { // if there is an improvement
                         distsTo[to] = newDistance;
                         preds[to] = first;
 
@@ -72,15 +79,13 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
                                 pred = preds[i];
                                 distsTo[pred] = i; // we use distsTo to store the successors of the vertexes found
                             }
-                            // TODO: implement boolean array to O(N) (M is number of discovery vertices)
-                            // TODO: ask assistant if okay to put time complexity before spatial complexity
-                            // TODO: add weight
+
                             while (circuitVertices.getLast() != pred) {
                                 circuitVertices.removeLast();
                             }
 
                             int weight = 0;
-                            for (int vertex : circuitVertices) {
+                            for (int vertex : circuitVertices) { // compute the weight of the absorbant circuit
                                 for (Edge outEdge : graph.getOutgoingEdges(vertex)) {
                                     if (outEdge.to() == distsTo[vertex]) {
                                         weight += outEdge.weight();
@@ -92,7 +97,7 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
 
                         }
 
-                        if (!contains[to]) {
+                        if (!contains[to]) { // add the new vertex in verticesQueue if it is not
                             contains[to] = true;
                             verticesQueue.addFirst(to);
                         }
--- app/src/main/java/gre/lab2/groupK/Main.java
+++ app/src/main/java/gre/lab2/groupK/Main.java
@@ -8,11 +8,19 @@ import gre.lab2.graph.*;
 import java.io.IOException;
 
 public final class Main {
-    static final int DEFAULT_SOURCE = 0;
+    static final int DEFAULT_SOURCE = 0; // default source for the algorithm
+    static final int FILE_COUNT = 4; // number of files to work on
 
+    /**
+     * Read the files representing weighted directed graphs
+     * and compute the Bellman-Ford-Yens's algorithm on them
+     *
+     * @param args No argument is needed
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         System.out.println("Labo 2 - GRE");
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= FILE_COUNT; i++) {
             System.out.println("Calcul des arborescences de plus court chemin dans le réseau " + i + ".");
 
             WeightedDigraph graph = WeightedDigraphReader.fromFile("data/reseau" + i + ".txt");
@@ -21,10 +29,10 @@ public final class Main {
 
             var result = algo.compute(graph, DEFAULT_SOURCE);
 
-            if (result.isNegativeCycle() || graph.getNVertices() < 25)
+            if (result.isNegativeCycle() || graph.getNVertices() < 25) // condition to write the result of the algorithm
                 System.out.println(result + "\n");
             else
-                System.out.println("Le réseau " + i + " possède une arborescence de plus court chemin.\n");
+                System.out.println("Le réseau " + i + " possède une arborescence de plus court chemin.\n"); //default message
 
         }
         // TODO
